16fd6e579c1e2457e0b713f1cd0230d6244b6f6a
Ehab Mohamed <ehabmohamed1481@gmail.com>
2015-08-15 15:17:01 +0400

    Code without comments

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,61 @@
+library(dplyr)
 setwd("F:\\DataScience\\Git\\repositories\\TidyData")
+dataSetPath <- "./UCI HAR Dataset"
+trainingFolder <- "train"
+testFolder <- "test"
+
+trainingSetFile <- paste(dataSetPath, trainingFolder, "X_train.txt", sep = "/")
+trainingSet <- read.table(trainingSetFile)##), nrows = 500)
+
+trainingLabelsFile <- paste(dataSetPath, trainingFolder, "y_train.txt", sep = "/")
+trainingLabels <- read.table(trainingLabelsFile)##, nrows = 500)
+
+trainingSubjectsFile <- paste(dataSetPath, trainingFolder, "subject_train.txt", sep = "/")
+trainingSubjects <- read.table(trainingSubjectsFile)##, nrows = 500)
+
+
+
+testSetFile <- paste(dataSetPath, testFolder, "X_test.txt", sep = "/")
+testSet <- read.table(testSetFile)##, nrows = 500)
+
+testLabelsFile <- paste(dataSetPath, testFolder, "y_test.txt", sep = "/")
+testLabels <- read.table(testLabelsFile)##, nrows = 500)
+
+testSubjectsFile <- paste(dataSetPath, testFolder, "subject_test.txt", sep = "/")
+testSubjects <- read.table(testSubjectsFile)##, nrows = 500)
+
+
+featuresFile <- paste(dataSetPath, "features.txt", sep = "/")
+features <- read.table(featuresFile)##, nrows = 2)
+
+
+activitiesFile <- paste(dataSetPath, "activity_labels.txt", sep = "/")
+activities <- read.table(activitiesFile)
+
+
+dataSet <- rbind(trainingSet, testSet)
+labels <- rbind(trainingLabels, testLabels)
+subjects <- rbind(trainingSubjects, testSubjects)
+
+
+
+features <- features[grepl("mean", tolower(features$V2)) | grepl("std", tolower(features$V2)), ]
+features$V1 <- paste0("V", features$V1)
+
+dataSet <- dataSet[, features$V1]
+names(dataSet) <- features$V2
+
+
+labels <- merge(labels, activities)
+
+dataSet <- cbind(labels$V2, subjects, dataSet)
+names(dataSet)[1] <- "activity"
+names(dataSet)[2] <- "subject"
+
+dataSet[1:35,1:5]
+
+summaryDataSet <- dataSet %>% group_by(activity, subject) %>% summarise_each(funs(mean))
+
+summaryDataSet <- data.frame(summaryDataSet)
+
+summaryDataSet[1:35,1:5]
